a84959385cbfd372509db8d0256ea3e4b5c56e1b
Engr.Aman Ullah <Engr.Aman Ullah@DESKTOP-RK06A4P>
2024-01-29 13:11:07 +0500

    Add project files.

---
+++ JwtAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JwtAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost("register")]
+        public Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            return Task.FromResult<ActionResult<User>>(Ok(user));
+        }
+
+        [HttpPost("login")]
+        public Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return Task.FromResult<ActionResult<string>>(BadRequest("User not Found."));
+            }
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return Task.FromResult<ActionResult<string>>(BadRequest("Wrong Password"));
+            }
+
+            string token = CreateToken(user, Get_configuration());
+            return Task.FromResult<ActionResult<string>>(Ok(token));
+        }
+
+        private IConfiguration Get_configuration()
+        {
+            return _configuration;
+        }
+
+        private string CreateToken(User user, IConfiguration _configuration)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var keyString = _configuration.GetSection("AppSettings:Token")?.Value;
+            if (string.IsNullOrEmpty(keyString))
+            {
+                throw new ArgumentNullException("AppSettings:Token is missing or empty.");
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
+
+            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            //    _configuration.GetSection("AppSettings:Token")?.Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            
+            return jwt;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            };
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512(user.PasswordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
---
+++ JwtAuthentication/User.cs
@@ -0,0 +1,13 @@
+﻿namespace JwtAuthentication
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+
+
+    }
+}
